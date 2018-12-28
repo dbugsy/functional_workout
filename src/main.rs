04e3a03c2ba3393b822d46394c8d7a7544d7fb17
Daniel Le Dosquet-Bergquist <dpbergquist@gmail.com>
2018-12-28 09:19:58 +0000

    simulate expensive calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    simulated_expensive_calculation(1);
+}
+
+fn simulated_expensive_calculation(intensity: i32) -> i32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
